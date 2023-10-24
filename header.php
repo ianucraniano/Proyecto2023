c5e1932ce3d0cf3ca084aa278032013b9bd9594c
NailRak <raknail01@gmail.com>
2023-10-24 19:39:28 -0300

    nueva actualizacion

--- header.php
+++ header.php
@@ -1,53 +1,44 @@
-
-<header>
-     <!-- Encabezado -->       
-    <div class="container-fluid">
-        <div class="row">
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https: //fonts.googleapis.com/css2? family= Roboto+Condensed:wght@700 & family= Roboto+Slab & display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> 
     
-          <div class="col-1">
-            <div class="my-3 ms-3"><a href="index.php"><img src="imagenes/logo.png" alt="logo" id="img1"></a>
-        </div>
-            </div>
-      <div class="col-11">
-        
-      <h1 class="">Museo Ferroviario</h1>
-      <h2>San Cristobal</h2></div>
-      </div>
-      </div>
-      </div>
-
-       <!-- Menú de Navegación -->  
-      <nav class="navbar navbar-expand-md navbar-light bg-light">
-      <div class="container-fluid">
+    <link rel="stylesheet" href="./CSS/estilo.css">
+    <title>Document</title>
+</head>
+<body>
     
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        
-        <li class="nav-item">
-          <a class="nav-link" href="index.php"><i class="bi bi-house"></i></a>
-        </li>
-
-       <!-- <li class="nav-item">
-          <a class="nav-link" href="error404.php">Novedades</a>
-        </li> -->
-         
-       </ul>
-        <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="invitado.php">Listado</a>
-
-        </li>	
-        <li class="nav-item">
-          <a class="nav-link" href="login.php">Administracion</a>
-        </li>
-        </ul>
+    <header>
+    <nav>
+      <div>
+          <img src="" alt="" width="30" height="24">
+        <a class="logo" href="/">Museo Sancris</a>
       </div>
+      <div class="mobile-menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
       </div>
-      </nav>       
-</header>
+      <ul class="nav-list">
+        <li><a href="#">Início</a></li>
+        <li><a href="#">Museo</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Proyectos</a></li>
+        <li><a href="#">Contato</a></li>
+        <li><a href="login.php">Ingresar</a></li>
+      </ul>
+    </nav>
     
     
+    
+    
+    </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+    <script src="./js/navbar.js"></script>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,87 +1,189 @@
-
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Validacion Datos Formulario</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/estilo.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet"> 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-</head>
-<body>
- 
- 
-   <?php
-     
-     include('header.php');
-
-   ?>
-      
-            
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Inicio</title>
+    <link href="https: //fonts.googleapis.com/css2? family= Roboto+Condensed:wght@700 & family= Roboto+Slab & display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> 
+<link href="https://fonts.googleapis.com/css2?family=Gabarito&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./CSS/estilo.css">
+    <link rel="stylesheet" href="./CSS/contacto.css">
     
-   <!-- Index.php contiene un Formulario --> 
+  </head>
+
+  <body>
+    <?php
+
+    include("header.php");
+    ?>
+    
+    <main>
+        <div class="carrusel mb-5 pb-5">
+
+            <div id="carouselExampleInterval" class="carousel slide container" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="./imagenes/imagen22.jpg" class="d-block w-100 imagenes" alt="...">
+                  </div>
+                  <div class="carousel-item" data-bs-interval="2000">
+                    <img src="./imagenes/imagen3.jpg" class="d-block w-100 imagenes" alt="...">
+                  </div>
+                 
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+        </div>
+    </main>
 
    
-   
-  <section>
-   
-  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="imagenes/carousel1.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="imagenes/carousel2.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="imagenes/carousel3.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+
+<section class= " principal">
+
+
+<div class= "bienvenido">
+  <h1><strong>¡Bienvenido</strong> al museo de San Cristobal!</h1>
+  
 </div>
-  
-    
-  
+<div class="bienvenido">
+
+  <h2>PLANEA TU VISITA</h2>
+
+
+</div>
 
 
 
-  </section>
+</section>
 
-  <?php
-    include('footer.php');
-  ?>
-   
-   <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 
-</body>
 
+
+
+
+
+<footer>
+
+<div class="contacto">
+  <div>
+
+    <h2><u>Contacto</u></h2>
+    <p><strong>Direccion</strong></p>
+    <p>Lisandro de la torre 1376</p>
+    <p>San Cristobal - Santa Fe - Argentina</p>
+    <p><strong>Telefono</strong></p>
+    <p>+54 3408-678965</p>
+  </div>
+  <div class="form-contacto">
+
+    <form action="">
+    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Nombre</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="escriba su nombre completo">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Comentario</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+    </form>
+  </div>
+</div>
+
+
+
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+    <script src="./js/navbar.js"></script>
+  </body>
 </html>
