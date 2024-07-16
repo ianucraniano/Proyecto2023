4bd08200232efae8781148cd6a6eb0a51c47f18e
Ian Rak 2.0 <ianrak46@gmail.com>
2024-07-15 21:33:38 -0300

    Refinamos un poco, todo listo

--- form_agregar.php
+++ form_agregar.php
@@ -12,7 +12,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/nav.css">
-    <title>Document</title>
+    <title>Agregar usuario</title>
 
      
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css"> 
--- menupiezas.php
+++ menupiezas.php
@@ -35,11 +35,11 @@ if(!empty($_POST["num"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Menu piezas</title>
 
   <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css"> 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-
+ 
 </head>
 <body>
 
